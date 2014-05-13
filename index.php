3d3357c5c8a66ad24575f8975de60ce28054b0c6
BlackScorp <witalimik@web.de>
2014-05-13 13:23:55 +0200

    fixed methods

--- index.php
+++ index.php
@@ -5,5 +5,4 @@ $app = require __DIR__ . '/bootstrap.php';
 $controller = new Controller_Guestbook($app['pdo'], $app['templatePath']);
 $controller->before();
 $controller->listEntries();
-$controller->newEntry();
 $controller->after();
